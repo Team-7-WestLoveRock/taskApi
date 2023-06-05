32c7a550db413ba44244a3bf9db4cf542490263e
mmm4707 <kim9905177@gmail.com>
2023-06-05 10:05:42 +0900

    configuration

---
+++ src/main/java/com/nhnacademy/westloverock/taskapi/TaskApiApplication.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.westloverock.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/westloverock/taskapi/TaskApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.westloverock.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+@SpringBootTest
+class TaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
