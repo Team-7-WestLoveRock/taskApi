729fe4982bb3ddeaa9cbd9500ba4c157bd611403
mmm4707 <kim9905177@gmail.com>
2023-06-06 02:38:51 +0900

    ✨Feat(TaskAuthorityTest) TaskAuthority entity Testcode 추가
    TaskAuthority Entity에 대한 모든 필드에 대해 getter와 setter를 테스트

---
+++ src/test/java/com/nhnacademy/westloverock/taskapi/entity/TaskAuthorityTest.java
@@ -0,0 +1,70 @@
+package com.nhnacademy.westloverock.taskapi.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@ActiveProfiles("dev")
+@SpringBootTest
+class TaskAuthorityTest {
+
+    private TaskAuthority taskAuthority;
+    private Task task;
+
+    @BeforeEach
+    void setUp() {
+        task = new Task();
+        TaskAuthority.Pk pk = new TaskAuthority.Pk(1, "seohyun");
+        taskAuthority = TaskAuthority.builder()
+                .id(pk)
+                .task(task)
+                .authority("Admin")
+                .build();
+    }
+    @Test
+    @DisplayName("작업 Getter 테스트")
+    void getTaskTest() {
+        assertEquals(task, taskAuthority.getTask());
+    }
+
+    @Test
+    @DisplayName("작업 Setter 테스트")
+    void setTaskTest() {
+        Task newTask = new Task();
+        taskAuthority.setTask(newTask);
+        assertEquals(newTask, taskAuthority.getTask());
+    }
+
+    @Test
+    @DisplayName("권한 Getter 테스트")
+    void getAuthorityTest() {
+        assertEquals("Admin", taskAuthority.getAuthority());
+    }
+
+    @Test
+    @DisplayName("권한 Setter 테스트")
+    void setAuthorityTest() {
+        taskAuthority.setAuthority("User");
+        assertEquals("User", taskAuthority.getAuthority());
+    }
+
+    @Test
+    @DisplayName("아이디 Getter 테스트")
+    void getIdTest() {
+        assertNotNull(taskAuthority.getId());
+    }
+
+    @Test
+    @DisplayName("아이디 Setter 테스트")
+    void setIdTest() {
+        TaskAuthority.Pk newPk = new TaskAuthority.Pk(2, "manseok");
+        taskAuthority.setId(newPk);
+        assertEquals(newPk, taskAuthority.getId());
+    }
+}
