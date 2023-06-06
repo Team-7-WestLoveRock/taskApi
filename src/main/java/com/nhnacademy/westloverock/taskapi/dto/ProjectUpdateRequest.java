fe14a8debd73a50fa95cc73d567501d7bf1dfed7
mmm4707 <kim9905177@gmail.com>
2023-06-07 03:54:35 +0900

    ✨Feat(ProjectUpdateRequest) Dto - ProjectUpdateRequest 추가
    Dto - ProjectUpdateRequest 추가

---
+++ src/main/java/com/nhnacademy/westloverock/taskapi/dto/ProjectUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.westloverock.taskapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProjectUpdateRequest {
+
+    private String name;
+    private String description;
+    private String state;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime createAt;
+}
