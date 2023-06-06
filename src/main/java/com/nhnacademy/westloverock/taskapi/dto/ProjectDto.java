9a4b2d62ca8e217a547829e6dae146d705808f89
mmm4707 <kim9905177@gmail.com>
2023-06-07 03:55:27 +0900

    🧠Refactor(ProjectDto) 날짜 패턴 json으로 변경
    ProjectDto 날짜 패턴 json으로 변경

--- src/main/java/com/nhnacademy/westloverock/taskapi/dto/ProjectDto.java
+++ src/main/java/com/nhnacademy/westloverock/taskapi/dto/ProjectDto.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.westloverock.taskapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,5 +15,8 @@ public class ProjectDto implements Serializable{
 
     private String name;
     private String description;
+    private String state;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime createAt;
 
 }
