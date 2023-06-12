7f10a71687eaa2852dfedfc30b18485eef7854fe
mmm4707 <kim9905177@gmail.com>
2023-06-12 15:43:06 +0900

    🎨Design(html) html파일에 메인페이지 링크 추가
    html파일에 메인페이지 링크 추가

--- src/main/java/com/nhnacademy/westloverock/taskapi/repository/TagRepository.java
+++ src/main/java/com/nhnacademy/westloverock/taskapi/repository/TagRepository.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.westloverock.taskapi.repository;
 
-import com.nhnacademy.westloverock.taskapi.entity.Project;
 import com.nhnacademy.westloverock.taskapi.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
